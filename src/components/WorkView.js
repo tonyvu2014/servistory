7ec9bd6dfde6c5391af1c9338cc3a7493b8ca2f0
Tony Vu <tonyvu2008@gmail.com>
2022-06-21 16:21:00 +1000

    Update WorkView

--- src/components/WorkView.js
+++ src/components/WorkView.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Box from "@mui/material/Box";
 import Stack from '@mui/material/Stack';
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import format from 'date-fns/format';
 import parseISO from 'date-fns/parseISO';
-import { DATE_PICKER_FORMAT } from '../common/constant';
+import { DATE_DISPLAY_FORMAT } from '../common/constant';
 
 const WorkView = (props) => {
 
@@ -14,93 +16,72 @@ const WorkView = (props) => {
     return (
         <Box>
             <Stack direction="row" spacing={2} sx={{ my: 3 }}>
-                <TextField 
-                    id="customer_name"
-                    name="customer_name"
-                    label="Customer Name"
-                    variant="outlined"
-                    defaultValue={work?.customer_name}
-                    InputProps={{
-                        readOnly: true
-                    }}
-                    fullWidth
-                />
-                <TextField 
-                    id="customer_phone"
-                    name="customer_phone"
-                    label="Customer Phone"
-                    variant="outlined"
-                    defaultValue={work?.customer_phone}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                    fullWidth
-                />
+                <Grid container>
+                    <Grid item xs={6}>
+                        <Box>
+                            <Typography variant="subtitle1">Customer Name</Typography>
+                            <Box component="div">
+                                <Typography variant="body1">{work.customer_name}</Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Box>
+                            <Typography variant="subtitle1">Customer Phone</Typography>
+                            <Box component="div">
+                                <Typography variant="body1">{work.customer_phone}</Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Grid>
             </Stack>
             <Stack direction="row" spacing={2} sx={{ my: 3 }}>
-                <TextField 
-                    id="plate_no"
-                    name="plate_no"
-                    label="Registration"
-                    variant="outlined"
-                    defaultValue={work?.plate_no}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                    fullWidth
-                />
-                <TextField 
-                    id="car_model"
-                    name="car_model"
-                    label="Vehicle Make and Model"
-                    variant="outlined"
-                    defaultValue={work?.plate_no}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                    fullWidth
-                />
+                <Grid container>
+                    <Grid item xs={6}>
+                        <Box>
+                            <Typography variant="subtitle1">Registration</Typography>
+                            <Box component="div">
+                                <Typography variant="body1">{work.plate_no}</Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Box>
+                            <Typography variant="subtitle1">Vehicle Make and Model</Typography>
+                            <Box component="div">
+                                <Typography variant="body1">{work.car_model}</Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Grid>
             </Stack>
             <Stack direction="row" spacing={2} sx={{ my: 3 }}>
-                <TextField 
-                    id="date_time_arrived"
-                    name="date_time_arrived"
-                    type="date"
-                    label="Drop-Off Date"
-                    variant="outlined"
-                    defaultValue={work?.date_time_arrived ? format(parseISO(work?.date_time_arrived), DATE_PICKER_FORMAT) : ''}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                    fullWidth
-                />
-                <TextField 
-                    id="date_time_pickup"
-                    name="date_time_pickup"
-                    type="date"
-                    label="Pick-Up Date"
-                    variant="outlined"
-                    defaultValue={work?.date_time_pickup ? format(parseISO(work?.date_time_pickup), DATE_PICKER_FORMAT) : ''}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                    fullWidth
-                />
+                <Grid container>
+                    <Grid item xs={6}>
+                        <Box>
+                            <Typography variant="subtitle1">Drop-Off Date</Typography>
+                            <Box component="div">
+                                <Typography variant="body1">{work?.date_time_arrived ? format(parseISO(work?.date_time_arrived), DATE_DISPLAY_FORMAT) : ''}</Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Box>
+                            <Typography variant="subtitle1">Pick-Up Date</Typography>
+                            <Box component="div">
+                                <Typography variant="body1">{work?.date_time_pickup ? format(parseISO(work?.date_time_pickup), DATE_DISPLAY_FORMAT) : ''}</Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Grid>
             </Stack>
             <Stack direction="row" spacing={2} sx={{ my: 3 }}>
-                <TextField 
-                    multiline
-                    minRows={5}
-                    id="note"
-                    name="note"
-                    label="Details/Notes"
-                    variant="outlined"
-                    defaultValue={work?.note}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                    fullWidth
-                />
+                <Box>
+                    <Typography variant="subtitle1">Details/Notes</Typography>
+                    <Box component="div">
+                        <Typography variant="body1">{work?.note}</Typography>
+                    </Box>
+                </Box>
             </Stack>
         </Box>
     )
