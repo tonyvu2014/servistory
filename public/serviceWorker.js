18fef415931ff15d40ccfb171b90efa20f30cf01
Tony Vu <tonyvu2008@gmail.com>
2022-08-09 22:25:56 +1000

    add notification click handler

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 
-// Any other custom service worker logic can go here.
+// handle push notification event receiver
 self.addEventListener('push', event => {
     const data = event.data.json();
 
@@ -9,4 +9,28 @@ self.addEventListener('push', event => {
         icon: 'https://dashboard.servistory.com/servistory.png',
         requireInteraction: true
     });
+});
+
+// handle notification click event
+self.addEventListener('notificationclick', (event) => {
+    const clickedNotification = event.notification;
+    clickedNotification.close();
+    let url = 'https://dashboard.servistory.com/';
+
+    event.waitUntil(
+        self.clients.matchAll().then(windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (self.clients.openWindow) {
+                return self.clients.openWindow(url);
+            }
+        })
+    );
 });
